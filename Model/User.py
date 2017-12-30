5a453802b76010acfe0ba9b754c62219aa5d9211
Divine <deguzouwa@yahoo.com>
2017-12-30 14:01:44 -0600

    added  to token authentication

--- Model/User.py
+++ Model/User.py
@@ -56,7 +56,7 @@ class User(db.Model):
 
         try:
             id, user, passwd, session_count = serializer.loads(session_token)
-            user = cls.query.filter_by(id=id, username=user, password=str.encode(passwd))
+            user = cls.query.filter_by(id=id, username=user, password=str.encode(passwd), session_count=session_count)
         except BadSignature:
             return None
 
